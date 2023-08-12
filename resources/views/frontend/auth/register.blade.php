97b9e7a64d456ffecda3878258f777a8cc9bc3c1
irozen-mazharul <141237975+irozen-mazharul@users.noreply.github.com>
2023-08-12 17:04:18 +0600

    modify register page

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -12,12 +12,12 @@
                 <div class="col-xl-12">
                     <div class="breadcrumb__content__wraper" data-aos="fade-up">
                         <div class="breadcrumb__title">
-                            <h2 class="heading">Log In</h2>
+                            <h2 class="heading">Register</h2>
                         </div>
                         <div class="breadcrumb__inner">
                             <ul>
-                                <li><a href="index.html">Home</a></li>
-                                <li>Log In</li>
+                                <li><a href="{{route('home')}}">Home</a></li>
+                                <li>Register</li>
                             </ul>
                         </div>
                     </div>
@@ -45,10 +45,10 @@
                 <div class="col-xl-8 col-md-8 offset-md-2" data-aos="fade-up">
                     <ul class="nav  tab__button__wrap text-center" id="myTab" role="tablist">
                         <li class="nav-item" role="presentation">
-                            <button class="single__tab__link active" data-bs-toggle="tab" data-bs-target="#projects__one" type="button">Login</button>
+                            <button class="single__tab__link active" data-bs-toggle="tab" data-bs-target="#projects__one" type="button">Sign Up</button>
                         </li>
                         <li class="nav-item" role="presentation">
-                            <button class="single__tab__link" data-bs-toggle="tab" data-bs-target="#projects__two" type="button">Sing up</button>
+                            <button class="single__tab__link" data-bs-toggle="tab" data-bs-target="#projects__two" type="button">Login</button>
                         </li>
                     </ul>
                 </div>
@@ -60,61 +60,8 @@
                         <div class="col-xl-8 col-md-8 offset-md-2">
                             <div class="loginarea__wraper">
                                 <div class="login__heading">
-                                    <h5 class="login__title">Login</h5>
-                                    <p class="login__description">Don't have an account yet? <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Sign up for free</a></p>
+                                    <h5 class="login__title">Sign Up</h5>
                                 </div>
-
-
-
-                                <form action="#">
-                                    <div class="login__form">
-                                        <label class="form__label">Username or email</label>
-                                        <input class="common__login__input" type="text" placeholder="Your username or email">
-
-                                    </div>
-                                    <div class="login__form">
-                                        <label class="form__label">Password</label>
-                                        <input class="common__login__input" type="password" placeholder="Password">
-
-                                    </div>
-                                    <div class="login__form d-flex justify-content-between flex-wrap gap-2">
-                                        <div class="form__check">
-                                            <input id="forgot" type="checkbox">
-                                            <label for="forgot"> Remember me</label>
-                                        </div>
-                                        <div class="text-end login__form__link">
-                                            <a href="#">Forgot your password?</a>
-                                        </div>
-                                    </div>
-                                    <div class="login__button">
-                                        <a class="default__button" href="#">Log In</a>
-                                    </div>
-                                </form>
-
-                                <div class="login__social__option">
-                                    <p>or Log-in with</p>
-
-                                    <ul class="login__social__btn">
-                                        <li><a class="default__button login__button__1" href="#"><i class="icofont-facebook"></i> Gacebook</a></li>
-                                        <li><a class="default__button" href="#"><i class="icofont-google-plus"></i> Google</a></li>
-                                    </ul>
-                                </div>
-
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="tab-pane fade" id="projects__two" role="tabpanel" aria-labelledby="projects__two">
-                        <div class="col-xl-8 offset-md-2">
-                            <div class="loginarea__wraper">
-                                <div class="login__heading">
-                                    <h5 class="login__title">Sing Up</h5>
-                                    <p class="login__description">Already have an account? <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Log In</a></p>
-                                </div>
-
-
-
                                 <form action="#">
                                     <div class="row">
                                         <div class="col-xl-6">
@@ -171,6 +118,53 @@
                                         <a class="default__button" href="#">Log In</a>
                                     </div>
                                 </form>
+
+                                <div class="login__social__option">
+                                    <p>or Log-in with</p>
+
+                                    <ul class="login__social__btn">
+                                        <li><a class="default__button login__button__1" href="#"><i class="icofont-facebook"></i> Gacebook</a></li>
+                                        <li><a class="default__button" href="#"><i class="icofont-google-plus"></i> Google</a></li>
+                                    </ul>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="tab-pane fade" id="projects__two" role="tabpanel" aria-labelledby="projects__two">
+                        <div class="col-xl-8 offset-md-2">
+                            <div class="loginarea__wraper">
+                                <div class="login__heading">
+                                    <h5 class="login__title">Login</h5>
+                                </div>
+
+                                <form action="#">
+                                    <div class="login__form">
+                                        <label class="form__label">Username or email</label>
+                                        <input class="common__login__input" type="text" placeholder="Your username or email">
+
+                                    </div>
+                                    <div class="login__form">
+                                        <label class="form__label">Password</label>
+                                        <input class="common__login__input" type="password" placeholder="Password">
+
+                                    </div>
+                                    <div class="login__form d-flex justify-content-between flex-wrap gap-2">
+                                        <div class="form__check">
+                                            <input id="forgot" type="checkbox">
+                                            <label for="forgot"> Remember me</label>
+                                        </div>
+                                        <div class="text-end login__form__link">
+                                            <a href="#">Forgot your password?</a>
+                                        </div>
+                                    </div>
+                                    <div class="login__button">
+                                        <a class="default__button" href="#">Log In</a>
+                                    </div>
+                                </form>
+
                             </div>
                         </div>
 
@@ -178,15 +172,12 @@
                 </div>
 
             </div>
-
             <div class=" login__shape__img educationarea__shape_image">
                 <img class="hero__shape hero__shape__1" src="{{ asset("asset/img")}}/education/hero_shape2.png" alt="Shape">
                 <img class="hero__shape hero__shape__2" src="{{ asset("asset/img")}}/education/hero_shape3.png" alt="Shape">
                 <img class="hero__shape hero__shape__3" src="{{ asset("asset/img")}}/education/hero_shape4.png" alt="Shape">
                 <img class="hero__shape hero__shape__4" src="{{ asset("asset/img")}}/education/hero_shape5.png" alt="Shape">
             </div>
-
-
         </div>
     </div>
 

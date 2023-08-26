8b7b94d69363841ab2800b9bca3044e187b5a1e0
irozen-mazharul <141237975+irozen-mazharul@users.noreply.github.com>
2023-08-26 17:27:19 +0600

    gallery image table restructure

--- database/migrations/2023_08_16_073809_create_galleries_table.php
+++ database/migrations/2023_08_16_073809_create_galleries_table.php
@@ -14,13 +14,17 @@ return new class extends Migration
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
-            $table->string('file_extension')->nullable();
-            $table->string('image_path');
+            $table->string('original_path');  // Original image
+            $table->string('sm_path')->nullable();  // Small version
+            $table->string('m_path')->nullable();   // Medium version
+            $table->string('l_path')->nullable();   // Large version
+            $table->string('file_extension');  // Extension of the original image
             $table->timestamps();
 
             $table->foreign('product_id')->references('id')->on('products');
         });
     }
+
 
     /**
      * Reverse the migrations.
